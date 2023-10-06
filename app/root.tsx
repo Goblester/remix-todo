d1da7cf14fef5c75198e09c81075c96ce7db50a5
Goblester <goblin829@gmail.com>
2023-10-06 19:25:26 +0300

    make contacts route

--- app/root.tsx
+++ app/root.tsx
@@ -1,55 +1,98 @@
 import {
-  Form,
-  Links,
-  LiveReload,
-  Meta,
-  Scripts,
-  ScrollRestoration,
+    Form, Link,
+    Links,
+    LiveReload,
+    Meta, Outlet,
+    Scripts,
+    ScrollRestoration, useLoaderData,
 } from "@remix-run/react";
+import type {LinksFunction} from "@remix-run/node";
+import appStylesHref from './app.css';
+import {getContacts} from "~/data";
+import {json} from "@remix-run/node";
+
+export const links: LinksFunction = () => [
+    {rel: "stylesheet", href: appStylesHref},
+];
+
+export const loader = async () => {
+    const contacts = await getContacts()
+
+    return json({contacts});
+}
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+    const {contacts} = useLoaderData<typeof loader>();
 
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <Meta/>
+            <Links/>
+        </head>
+        <body>
+        <div id="sidebar">
+            <h1>Remix Contacts</h1>
+            <div>
+                <Form id="search-form" role="search">
+                    <input
+                        id="q"
+                        aria-label="Search contacts"
+                        placeholder="Search"
+                        type="search"
+                        name="q"
+                    />
+                    <div id="search-spinner" aria-hidden hidden={true}/>
+                </Form>
+                <Form method="post">
+                    <button type="submit">New</button>
+                </Form>
+            </div>
+            <nav>
+                <ul>
+                    {contacts.length ?
+                        (<ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    <Link to={`contacts/${contact.id}`}>
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite ? (
+                                            <span>★</span>
+                                        ) : null}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>)
+                        :
+                        (<p>
+                            <i>No contacts</i>
+                        </p>)
+                    }
+                    <li>
+                        <Link to={`/contacts/1`}>Your Name</Link>
+                    </li>
+                    <li>
+                        <Link to={`/contacts/2`}>Your Friend</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div id="detail">
+            <Outlet/>
+        </div>
+        <ScrollRestoration/>
+        <Scripts/>
+        <LiveReload/>
+        </body>
+        </html>
+    );
 }
